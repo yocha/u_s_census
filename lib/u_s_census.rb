6c5022ce96562057dbb130d103fdb3db8b51d43f
Yong Cha <yocha@yahoo.com>
2012-06-08 01:14:44 -0400

    add FIPS info

--- lib/u_s_census.rb
+++ lib/u_s_census.rb
@@ -5,33 +5,46 @@ require "u_s_census/version"
 
 module USCensus
   HOST = "http://thedataweb.rm.census.gov/data/2010/"
+	# FIPS geography code
+	STATES = {
+		"Alabama"=>"01","Alaska"=>"02","Arizona"=>"04","Arkansas"=>"05","California"=>"06","Colorado"=>"08","Connecticut"=>"09","Delaware"=>"10","District of Columbia"=>"11","Florida"=>"12","Georgia"=>"13","Hawaii"=>"15","Idaho"=>"16","Illinois"=>"17","Indiana"=>"18","Iowa"=>"19","Kansas"=>"20","Kentucky"=>"21","Louisiana"=>"22","Maine"=>"23","Maryland"=>"24","Massachusetts"=>"25","Michigan"=>"26","Minnesota"=>"27","Mississippi"=>"28","Missouri"=>"29","Montana"=>"30","Nebraska"=>"31" ,"Nevada"=>"32","New Hampshire"=>"33","New Jersey"=>"34","New Mexico"=>"35","New York"=>"36","North Carolina"=>"37","North Dakota"=>"38","Ohio"=>"39","Oklahoma" =>"40","Oregon"=>"41","Pennsylvania"=>"42","Rhode Island"=>"44","South Carolina"=>"45","South Dakota"=>"46","Tennessee"=>"47","Texas"=>"48","Utah"=>"49","Vermont"=>"50","Virginia"=>"51","Washington"=>"53","West Virginia"=>"54","Wisconsin"=>"55","Wyoming"=>"56","Puerto Rico"=>"72"
+	}
 
 	# Get 2010 Census SF1 data for the states by using
-	# <tt>census_summary_2010</tt> and pass it key and list of states.
+	# <tt>census_summary_2010</tt> and pass it key and list of states (FIPS codes).
 	# This will return array of hashes.
-	def self.census_summary_2010(key, state = "*")
-		url = HOST + "sf1?key=#{key}&get=P0010001,NAME&for=state:#{state}"
+	def self.census_summary_2010(key, states = "*")
+		states = fips_codes(states)
+		url = HOST + "sf1?key=#{key}&get=P0010001,NAME&for=state:#{states}"
 		output = open(url)
 		return parse(output, key)
 	end
 	private
+	def self.fips_codes(states)
+		if states.is_a? Array
+			states.map do |s|
+				STATES[s] || s
+			end.join(',')
+		else
+			states
+		end
+	end
 	def self.parse(output, key)
-		status = output.status[1]
-		if 400 == status
+		status = output.status[0]
+		if "400" == status
 			raise "Request was not valid. Queried for unknown variables or unknown geographies: #{ouput.read}"
-		elsif 500 == status
+		elsif "500" == status
 			raise "Server side error while processing the request. Try again."
-		elsif 204 == status
+		elsif "204" == status
 			return []
-		elsif 200 == status
-			puts output.base_uri.to_s
+		elsif "200" == status
 			if output.base_uri.to_s.match /invalid_key/
 				raise "The key with this request it not valid: #{key}"
 			else
 		    return array_to_hash YAML.load output.read	
 			end
 		else
-			raise "Unknow status code: #{status}"
+			raise "Unknown status code: #{status}"
 		end
 	end
 	def self.array_to_hash(output)
